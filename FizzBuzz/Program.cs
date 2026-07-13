01abdc2ef33ada02b81e9626f73115cb532add5a
Al Palaran <aljade1902@gmail.com>
2026-07-13 10:09:12 +0100

    add fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program {
+  public static void Main(string[] args){
+    Console.WriteLine("Welcome to FizzBuzz");
+    Console.Write("How many numbers would you like: ");
+    string? input = Console.ReadLine();
+
+    if (input == null) {
+      Console.WriteLine("Please enter a valid number.");
+      return;
+    }
+    try
+    {
+      var numberRange = int.Parse(input);
+      for (int counter = 1; counter <= numberRange; counter++)
+      {
+        Console.WriteLine(FizzBuzz(counter));
+      }
+    }
+    catch (FormatException)
+    {
+      Console.WriteLine("Please enter a valid number.");
+      return;
+    }
+  }
+
+  static string FizzBuzz(int number)
+  {
+    if ((number % 3 == 0) && (number % 5 == 0)) {
+      return "FizzBuzz";
+    } else if (number % 3 == 0) {
+      return "Fizz";
+    } else if (number % 5 == 0) {
+      return "Buzz" ;
+    } else {
+      return number.ToString();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharpKatas
+This is a small collection of C# projects to help with getting familiar with C# code.
